358866d87bc5dc2e5d4a9a6fb5d39ad03525f6a3
Kaia Emilie <149586229+git-kaia@users.noreply.github.com>
2025-04-03 16:11:54 +0200

    Set up server connection * Server connection set up in server-connection.py * Configurations edited config.py

--- app/server-connection.py
+++ app/server-connection.py
@@ -1,1 +1,17 @@
 import pymysql
+from config import DB_CONFIG
+
+try:
+    conn = pymysql.connect(
+        host=DB_CONFIG["host"],
+        user=DB_CONFIG["user"],
+        password=DB_CONFIG["password"],
+        database=DB_CONFIG["database"],
+        port=DB_CONFIG["port"]
+    )
+    print("Connection to database successful.")
+
+
+except pymysql.MySQLError as err:
+    print(f"Error connecting to database: {err}")
+    conn.close()
