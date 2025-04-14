8833cfe43c4e2d1efeb0750e543f326217e6dc2f
git-kaia <149586229+git-kaia@users.noreply.github.com>
2025-04-14 22:59:12 +0200

    Changed server setup to SQLAlchemy with ORM * Changed setup in main.py and the config.py to go with SQLAlchemy setup * Deleted redundant server-connction.py file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from db import get_db
+from models.models import User
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Server in progress"}
+    return {"message": "Server is running"}
+
+@app.get("/users")
+def read_users(db: Session = Depends(get_db)):
+    users = db.query(User).all()
+    return users
