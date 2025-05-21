fcbcd3a133f0ab9d3f26dbca65b828e1bfa731ef
Jon P. <jonper@stud.ntnu.no>
2025-05-21 10:45:39 +0200

    Added NGINX-ports to main.py and the frontend * Added NGINX-ports to main.py. * Formatted main.py. * Updated the ports to the NGINX-port in main-page.js, profile.js, search_results.js, and shared.js.

--- frontend/frontend-scripts/shared.js
+++ frontend/frontend-scripts/shared.js
@@ -1,7 +1,7 @@
 const API_BASE_URL =
   
   // "https://idg2001-o1-social-networking-service.onrender.com"; // RENDER:
-  "http://127.0.0.1:8000"; // LOCAL:
+  "http://127.0.0.1:8080"; // LOCAL:
 
 // #####################
 // ### Local caching ###
